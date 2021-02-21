c60e9917bbc265297da2130a2379608cde77f174
Terence Li <terencedz.li@gmail.com>
2021-02-21 16:07:44 -0500

    Clean up install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
 
-if [ ! -d "~/zsh-syntax-highlighting" ];
-then
+if test ! $(which brew); then
+  echo "Installing homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+if [ ! -d ~/zsh-syntax-highlighting ]; then
   mkdir ~/zsh-syntax-highlighting
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh-syntax-highlighting 
 fi
 
-ln -s ~/Workspace/dotfiles/.zshrc .zshrc
-
-if [ ! -d "~/.config" ] && [ ! -d "~/.config/nvim" ];
-then
-  mkdir -p ~/.config/nvim
-  mkdir -p ~/.config/nvim/config
+if [ ! -f ~/.zshrc ]; then
+  ln -s ~/Workspace/dotfiles/.zshrc ~/.zshrc
+  source ~/.zshrc
 fi
 
+if [ ! -d ~/.config ] && [ ! -d ~/.config/nvim ]; then
+  mkdir -p ~/.config/nvim
+  mkdir -p ~/.config/nvim/config
 
-ln -s ~/Workspace/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-ln -s ~/Workspace/dotfiles/nvim/config/test.vim ~/.config/nvim/config/test.vim
-ln -s ~/Workspace/dotfiles/nvim/config/fzf.vim ~/.config/nvim/config/fzf.vim
+  ln -s ~/Workspace/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+  ln -s ~/Workspace/dotfiles/nvim/config/test.vim ~/.config/nvim/config/test.vim
+  ln -s ~/Workspace/dotfiles/nvim/config/fzf.vim ~/.config/nvim/config/fzf.vim
+fi
 
-source .zshrc
