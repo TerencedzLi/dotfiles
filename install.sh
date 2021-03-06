5ba98015206b786b2d0ad0f594228e4f58da0cba
Terence Li <terence.li@shopify.com>
2021-03-06 13:26:28 -0500

    Add spin specific installations

--- install.sh
+++ install.sh
@@ -1,10 +1,33 @@
 #!/bin/sh
 
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if [ $SPIN ];
+then
+  if ! command -v fzf &> /dev/null; then
+    sudo apt-get install -y fzf
+  fi
 
-  brew bundle
+  if ! command -v rg &> /dev/null; then
+    sudo apt-get install -y ripgrep
+  fi
+
+  if ! command -v nvim &> /dev/null; then
+    sudo apt install neovim
+  fi
+
+  if ! command -v ctags &> /dev/null; then
+    sudo apt install exuberant-ctags
+  fi
+else
+  if ! command -v brew &> /dev/null; then
+    echo "Installing homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+    brew bundle
+  fi
+fi
+
+if [ ! -d ~/.oh-my-zsh ]; then
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 fi
 
 if [ ! -d ~/zsh-syntax-highlighting ]; then
@@ -13,7 +36,7 @@ if [ ! -d ~/zsh-syntax-highlighting ]; then
 fi
 
 if [ ! -f ~/.zshrc ]; then
-  ln -s ~/Workspace/dotfiles/.zshrc ~/.zshrc
+  ln -sf ~/Workspace/dotfiles/.zshrc ~/.zshrc
   source ~/.zshrc
 fi
 
@@ -21,14 +44,14 @@ if [ ! -d ~/.config ] && [ ! -d ~/.config/nvim ]; then
   mkdir -p ~/.config/nvim
   mkdir -p ~/.config/nvim/config
 
-  ln -s ~/Workspace/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
-  ln -s ~/Workspace/dotfiles/nvim/config/test.vim ~/.config/nvim/config/test.vim
-  ln -s ~/Workspace/dotfiles/nvim/config/fzf.vim ~/.config/nvim/config/fzf.vim
+  ln -sf ~/Workspace/dotfiles/nvim/init.vim ~/.config/nvim/init.vim
+  ln -sf ~/Workspace/dotfiles/nvim/config/test.vim ~/.config/nvim/config/test.vim
+  ln -sf ~/Workspace/dotfiles/nvim/config/fzf.vim ~/.config/nvim/config/fzf.vim
 fi
 
 
 if [ ! -f ~/.tmux.conf ]; then
-  ln -s ~/Workspace/dotfiles/.tmux.conf ~/.tmux.conf
+  ln -sf ~/Workspace/dotfiles/.tmux.conf ~/.tmux.conf
 fi
 
 
